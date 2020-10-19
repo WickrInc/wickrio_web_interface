e7757795f979339215712d822a88be8dc4be86fa
Greg Messina <gregmessina@MacBook-Pro.local>
2020-10-19 14:02:25 -0400

    seperate config tokens json file

--- configure.js
+++ configure.js
@@ -43,70 +43,11 @@ main();
 
 async function main()
 {
-    const tokenConfig = [
-        {
-            token: 'BOT_PORT',
-            pattern: '^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$',
-            type: 'number',
-            description: "Please enter your client bot's port",
-            message: 'Cannot leave empty! Please enter a value',
-            required: false,
-            default: 'N/A',
-        },
-        {
-            token: 'BOT_API_KEY',
-            pattern: '',
-            type: 'string',
-            description: "Please enter your client bot's API-Key",
-            message: 'Cannot leave empty! Please enter a value',
-            required: true,
-            default: 'N/A',
-        },
-        {
-            token: 'BOT_API_AUTH_TOKEN',
-            pattern: '',
-            type: 'string',
-            description: 'Please create an Web API Basic Authorization Token, we recommend an alphanumeric string with at least 24 characters',
-            message: 'Cannot leave empty! Please enter a value',
-            required: true,
-            default: 'N/A',
-        },
-        {
-            token: 'HTTPS_CHOICE',
-            pattern: 'yes|no',
-            type: 'string',
-            description: 'Do you want to set up an HTTPS connection with the Web API Interface, highly recommended',
-            message: 'Please enter either yes or no',
-            required: true,
-            default: 'no',
-            list: [
-                {
-                    token: 'SSL_KEY_LOCATION',
-                    pattern: '',
-                    type: 'file',
-                    description: 'Please enter the name and location of your SSL .key file',
-                    message: 'Cannot find file!',
-                    required: true,
-                    default: 'N/A',
-                },
-                {
-                    token: 'SSL_CRT_LOCATION',
-                    pattern: '',
-                    type: 'file',
-                    description: 'Please enter the name and location of your SSL .crt file',
-                    message: 'Cannot find file!',
-                    required: true,
-                    default: 'N/A',
-                }
-            ]
-        }
-    ];
-
-
+    const tokens = require('./configTokens.json')
     var fullName = process.cwd() + "/processes.json";
-    wickrIOConfigure = new WickrIOBotAPI.WickrIOConfigure(tokenConfig, fullName, false, false);
+    wickrIOConfigure = new WickrIOBotAPI.WickrIOConfigure(tokens.tokens, fullName, tokens.supportAdministrators, tokens.supportVerification);
 
-    await wickrIOConfigure.configureYourBot("WickrIO-Web-Interface");
+    await wickrIOConfigure.configureYourBot(tokens.integration);
     process.exit();
 }
 
