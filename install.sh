4ea725d6e2d7ea72cd8c7813444042629dea6c64
Paul Cushman <paulcus@amazon.com>
2024-09-19 12:38:56 +0000

    update thelock file and the install.sh to use the node zeip fie if it exists

--- install.sh
+++ install.sh
@@ -4,4 +4,9 @@ if [ -f "/usr/local/nvm/nvm.sh" ]; then
   # nvm use 18
 fi
 
-npm install
+if [ -f ./node_modules.tgz ]
+then
+    tar -xvf ./node_modules.tgz
+else
+    npm install --unsafe-perm
+fi
